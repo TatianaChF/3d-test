22e31c7ad6dd6ceed2c706f2c0883c3db36399f6
TatianaChF <tatiana.chefox@gmail.com>
2026-03-05 14:15:45 +0300

    add color

--- src/cube.js
+++ src/cube.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
 
-export function createCube() {
+export function createCube(color) {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshLambertMaterial({color: '#00FF00'});
+    const material = new THREE.MeshLambertMaterial({color: color});
 
     return new THREE.Mesh(geometry, material);
 }
